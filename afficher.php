17ae568d41a75dcf4c00ad4da26a86fd70da5fa4
corentin <corentin.berthon@groupe-esigelec.org>
2024-04-16 14:18:33 +0200

    .

--- afficher.php
+++ afficher.php
@@ -1,7 +1,8 @@
 <?php
 function afficherData($csvFileName){
     // Lire le contenu du fichier CSV dans un tableau
-    $lines = file($csvFileName, FILE_IGNORE_NEW_LINES);
+
+    $lines = file("../".$csvFileName, FILE_IGNORE_NEW_LINES);
 
     // Vérifier si la fonction file() a réussi à lire le fichier
     if ($lines === false) {
