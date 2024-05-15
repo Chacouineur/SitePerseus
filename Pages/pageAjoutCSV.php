3f931ff97b5d29863a4ff30581c1b1527041ab57
Mael <mael@gmail.com>
2024-05-15 16:59:30 +0200

    ok

--- Pages/pageAjoutCSV.php
+++ Pages/pageAjoutCSV.php
@@ -7,7 +7,7 @@
     include '../rechercheConfig.php';
     unset($_SESSION['csvName']);
     unset($_SESSION['dataConfig']);
-    unset($_SESSION['nomCOnfig']);
+    unset($_SESSION['nomConfig']);
 
     // Vérifier si la variable de session 'csvFileName' est définie
     if(isset($_SESSION['csvFileName']) && isset($_SESSION['configName'])){
@@ -171,10 +171,6 @@
                     ?>
                 </tbody>
             </table>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
-            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
-
             <script>
                 var expanded = false;
                 function showCheckBoxes(){
